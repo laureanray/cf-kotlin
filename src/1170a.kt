bea55ec01ff5a1a0e5e764a62b7a9065dcbc7c91
unknown <laureanraybahala@gmail.com>
2020-12-16 05:44:02 +0800

    accepted solution 1170a, removed out folder

--- src/1170a.kt
+++ src/1170a.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>) {
-    println("Hello")
+    val q: Int? = readLine()?.toInt()
+    for (i in 0 until q!!) {
+        val (x: Int, y: Int) = readLine()!!
+            .split(' ')
+            .map { input-> input.toInt() }
+            .sorted()
+
+        println("1 ${x - 1} ${y - x + 1}")
+    }
 }
